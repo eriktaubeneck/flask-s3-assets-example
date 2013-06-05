dbb19a29cf9e2d217fc27a99f32ef899efbec670
Your Name <erik.taubeneck@gmail.com>
2013-06-05 12:05:17 -0400

    added S3_CDN_DOMAIN

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,8 @@ app.config['AWS_ACCESS_KEY_ID'] = 'aws_key'
 app.config['AWS_SECRET_ACCESS_KEY'] = 's3_secret'
 app.config['S3_BUCKET_NAME'] = 'why_not_zoidberg'
 app.config['DEBUG'] = True
+app.config['USE_S3_DEBUG'] = True
+app.config['S3_CDN_DOMAIN'] = 'd14tm1m1qgdu96.cloudfront.net'
 
 s3 = FlaskS3(app)
 
