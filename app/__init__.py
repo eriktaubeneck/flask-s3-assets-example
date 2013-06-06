bc0c1eeed49ef11a532be0604304cdd1c8462ecf
Your Name <erik.taubeneck@gmail.com>
2013-06-06 11:00:48 -0400

    added flask-assets

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_s3 import FlaskS3
+from flask.ext.assets import Environment, Bundle
 app = Flask(__name__)
 
 app.config['AWS_ACCESS_KEY_ID'] = 'aws_key'
@@ -10,6 +11,11 @@ app.config['USE_S3_DEBUG'] = True
 app.config['S3_CDN_DOMAIN'] = 'd14tm1m1qgdu96.cloudfront.net'
 
 s3 = FlaskS3(app)
+assets = Environment(app)
+
+js = Bundle('test.js', output='gen/packed.js')
+assets.register('js_all', js)
+
 
 @app.route("/")
 def index():
